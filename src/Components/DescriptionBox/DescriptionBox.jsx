54d2a78116b5e49b85f6f8554ab9d39a5ca81a33
Mir-hunza <130244140+Mir-Hunza@users.noreply.github.com>
2024-03-01 23:18:13 +0500

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './DescriptionBox.css'
+
+const DescriptionBox = () =>{
+    return(
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Step into sophistication with our range of  dresses designed to exude confidence and style. From casual outings to formal events, our collection seamlessly blends versatility with fashion-forward trends. Crafted from high-quality fabrics, each dress ensures a comfortable fit that complements your unique sense of style.</p>
+                <p>Indulge in the perfect blend of elegance and comfort with our dresses. From chic cocktail dresses to casual daywear, our collection caters to every occasion. Embrace the latest fashion trends with our carefully curated designs, ensuring you stand out wherever you go. Whether you prefer classic silhouettes or contemporary cuts, our dresses are a symbol of timeless beauty.</p>
+
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default DescriptionBox
